2e08a7243fea8aa46374746645f58aaac57d99c2
Pratik Chigani <pratikchigani@gmail.com>
2023-10-24 16:23:07 +0530

    Adding feature to use newapi url to fetch the news

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
               DailyDose
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -18,34 +18,41 @@ export default class Navbar extends Component {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
                     Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/" About>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
                     About
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="Contact">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active"
+                    aria-current="page"
+                    href="Contact"
+                  >
                     Contact
                   </a>
                 </li>
               </ul>
-              <form class="d-flex" role="search">
+              <form className="d-flex" role="search">
                 <input
-                  class="form-control me-2"
+                  className="form-control me-2"
                   type="search"
                   placeholder="Search"
                   aria-label="Search"
                 />
-                <button class="btn btn-outline-success" type="submit">
+                <button className="btn btn-outline-success" type="submit">
                   Search
                 </button>
               </form>
--- src/components/News.js
+++ src/components/News.js
@@ -2,58 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-  articles = [
-    {
-      source: {
-        id: "bbc-sport",
-        name: "BBC Sport",
-      },
-      author: null,
-      title: "Why do crowds look so low at Cricket World Cup?",
-      description:
-        "BBC Sport analyses why so many matches at the Cricket World Cup have been poorly attended so far.",
-      url: "http://www.bbc.co.uk/sport/cricket/67118882",
-      urlToImage:
-        "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/1094D/production/_131471976_gettyimages-1707881409.jpg",
-      publishedAt: "2023-10-20T14:22:22.3004629Z",
-      content:
-        "Empty stands have been a feature of Cricket World Cup games in India which don't feature the hosts\r\nTwo weeks in and, on the pitch, the Cricket World Cup in India is bubbling away nicely.\r\nShock resu… [+4532 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
+  articles = [];
 
   constructor() {
     super();
@@ -61,41 +10,45 @@ export default class News extends Component {
       articles: this.articles,
       loading: false,
     };
+    console.log("constructor");
   }
+
+  async componentDidMount() {
+    console.log("componentDidMount");
+    let url =
+      "https://newsapi.org/v2/top-Headlines?country=in&apiKey=3e768e74275f424cb172d2818919cedd";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+    // console.log(parsedData);
+  }
+
   render() {
+    console.log("render");
+    // let { url, title, description, urlToImage } = this.props;
     return (
       <div className="container mt-3">
         <h1 style={{ marginLeft: "400px" }}>DailyDose - Top Headlines</h1>
         <hr />
         <div className="row mt-5">
-          <div className="col-md-3">
-            <NewsItem
-              title="Mytitle"
-              description="Mydescription"
-              imageUrl="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/1094D/production/_131471976_gettyimages-1707881409.jpg"
-            />
-          </div>
-          <div className="col-md-3">
-            <NewsItem
-              title="Mytitle"
-              description="Mydescription"
-              imageUrl="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/1094D/production/_131471976_gettyimages-1707881409.jpg"
-            />
-          </div>
-          <div className="col-md-3">
-            <NewsItem
-              title="Mytitle"
-              description="Mydescription"
-              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            />
-          </div>
-          <div className="col-md-3">
-            <NewsItem
-              title="Mytitle"
-              description="Mydescription"
-              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"
-            />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : " "}
+                  description={
+                    element.description ? element.description.slice(0, 50) : " "
+                  }
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://cdn.ndtv.com/common/images/ogndtv.png"
+                  }
+                  url={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
