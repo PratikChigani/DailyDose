fa10692e91cd82aefbb45681991cc1cc91f8d4e8
Pratik Chigani <pratikchigani@gmail.com>
2023-11-14 23:24:06 +0530

    Incorporated Spinner feature

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export default class News extends Component {
   constructor(props) {
@@ -24,11 +25,13 @@ export default class News extends Component {
   async componentDidMount() {
     // let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=3e768e74275f424cb172d2818919cedd&pageSize=20&page=1`;
     let url = `https://newsapi.org/v2/top-Headlines?country=in&apiKey=3e768e74275f424cb172d2818919cedd&pageSize=${this.props.pageSize}&page=1`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
@@ -39,11 +42,13 @@ export default class News extends Component {
     // let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=3e768e74275f424cb172d2818919cedd&pageSize=20&page=${
     //   this.state.page - 1
     // }`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       page: this.state.page - 1,
+      loading: false,
     });
   };
 
@@ -54,11 +59,13 @@ export default class News extends Component {
     // let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=3e768e74275f424cb172d2818919cedd&pageSize=20&page=${
     //   this.state.page + 1
     // }`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       page: this.state.page + 1,
+      loading: false,
     });
   };
 
@@ -68,21 +75,25 @@ export default class News extends Component {
       <div className="container mt-3">
         <h1 className="text-center">DailyDose - Top Headlines</h1>
         <hr />
+        {this.state.loading && <Spinner />}
         <div className="row mt-5">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-3" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 45) : " "}
-                  description={
-                    element.description ? element.description.slice(0, 50) : " "
-                  }
-                  imageUrl={element.urlToImage ? element.urlToImage : ""}
-                  url={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-3" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : " "}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 50)
+                        : " "
+                    }
+                    imageUrl={element.urlToImage ? element.urlToImage : ""}
+                    url={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between my-5">
           <button
